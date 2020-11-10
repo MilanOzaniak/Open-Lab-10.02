599bf6424179779f9ce3b61818b4fe93b083de1c
Ozaniak <Ozaniak@PC>
2020-11-10 09:51:14 +0100

    hotovo

--- Open-Lab-10.02/Program.cs
+++ Open-Lab-10.02/Program.cs
@@ -7,33 +7,33 @@ namespace Open_Lab_10._02
     {
         public class Book
         {
-            private string Title;
-            private string Author;
-            private string Category;
-            private int Pages;
-            private int ReleaseDate;
+            private string title;
+            private string author;
+            private string category;
+            private int pages;
+            private int releaseDate;
 
-            public string _Title
+            public string Title
             {
                 get;
                 set;
             }
-            public string _Author
+            public string Author
             {
                 get;
                 set;
             }
-            public string _Category
+            public string Category
             {
                 get;
                 set;
             }
-            public int _Pages
+            public int Pages
             {
                 get;
                 set;
             }
-            public int _ReleaseDate
+            public int ReleaseDate
             {
                 get;
                 set;
@@ -43,18 +43,18 @@ namespace Open_Lab_10._02
         static void Main(string[] args)
         {
             Book book = new Book();
-            book._Title = "The Fellowship of the Ring";
-            book._Author = "J. R. R. Tolkien";
-            book._Category = "fantasy and adventure";
-            book._Pages = 300;
-            book._ReleaseDate = 1954;
+            book.Title = "The Fellowship of the Ring";
+            book.Author = "J. R. R. Tolkien";
+            book.Category = "fantasy and adventure";
+            book.Pages = 300;
+            book.ReleaseDate = 1954;
             
             
-            Console.WriteLine($"Title: {book._Title} ");
-            Console.WriteLine($"Author: {book._Author} ");
-            Console.WriteLine($"Release date: {book._ReleaseDate} ");
-            Console.WriteLine($"Category of the book: {book._Category} ");
-            Console.WriteLine($"Number of pages: {book._Pages} ");
+            Console.WriteLine($"Title: {book.Title} ");
+            Console.WriteLine($"Author: {book.Author} ");
+            Console.WriteLine($"Release date: {book.ReleaseDate} ");
+            Console.WriteLine($"Category of the book: {book.Category} ");
+            Console.WriteLine($"Number of pages: {book.Pages} ");
 
         }
     }
